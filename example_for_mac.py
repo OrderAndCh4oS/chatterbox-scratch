2313ecea8ab68918914187a03fbe1726715c4fac
Sean Cooper <scooper@adaptavist.com>
2025-06-02 16:42:01 +0100

    set mac example text

--- example_for_mac.py
+++ example_for_mac.py
@@ -17,13 +17,15 @@ def patched_torch_load(*args, **kwargs):
 torch.load = patched_torch_load
 
 model = ChatterboxTTS.from_pretrained(device=device)
-text = "Toto, I've a feeling we're not in Kansas anymore."
+text = ("Best sandwich I ever tasted I found in a plastic bag under a train seat I don't know what was in it orangey "
+        "yellow sticky stuff it was sort of quite Tangy with a kind of fishy after taste I still think of that "
+        "sandwich sometimes at night if I can't sleep yum yum.")
 
 AUDIO_PROMPT_PATH = "voice-one.wav"
 wav = model.generate(
     text, 
     audio_prompt_path=AUDIO_PROMPT_PATH,
-    exaggeration=0.25,
-    cfg_weight=0.25
+    exaggeration=0.35,
+    cfg_weight=0.5
     )
 ta.save("output-1.wav", wav, model.sr)
